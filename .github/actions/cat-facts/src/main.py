4d6ce2d01858bca70d66d1ea5b1a8b7e38a4eb70
Łukasz Mitka <75326685+lukaszmitka-aptiv@users.noreply.github.com>
2020-12-22 11:13:25 +0100

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -20,7 +20,7 @@ print(fact_list)
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    return fact_arr[random.choice(fact_arr)]
+    return random.choice(fact_arr)
 
 random_fact = select_random_fact(fact_list)
 
